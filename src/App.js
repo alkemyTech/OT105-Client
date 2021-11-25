241bb2db60084978b22cf14e4705dbf4b3b6dab8
Gabriel Shimabuku <gaby.shima12@gmail.com>
2021-11-25 14:05:56 -0300

    Added MUI library. Added Roboto font in index.html. Deleted unnecesary files from CRA redux template (features folder, logo.svg, some lines from store.js). Added Title component which receives 7 props: 1 required, 6 optional

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import ActivitiesForm from './Components/Activities/ActivitiesForm';
@@ -13,6 +12,7 @@ import SchoolCampaign from './Campaigns/School/SchoolCampaign';
 import ToysCampaign from './Campaigns/Toys/ToysCampaign';
 import MembersForm from './Components/Members/MembersForm';
 import ProjectsForm from './Components/Projects/ProjectsForm';
+import Title from './Components/Title/Title';
 
 function App() {
   return (

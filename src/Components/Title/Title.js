9452c093bd0605022f421559adcb7ca6fe04ebc1
Gabriel Shimabuku <gaby.shima12@gmail.com>
2021-11-26 16:02:35 -0300

    Removed unnecessary empty lines. Changed some props names: from text to titleText, color to titleTextColor and variant to titleMuiVariant. Removed documentation comments

--- src/Components/Title/Title.js
+++ src/Components/Title/Title.js
@@ -2,24 +2,13 @@ import React from 'react'
 
 //Styles
 import s from './Title.module.css'
-
 //MUI
 import { Typography } from '@mui/material'
 
-//Title component receives 7 props: text, color, fontSize, imageUrl, bckgColor, bckgOpacity and paddingBlock
-//text (required) : The text of the title
-//color (optional): Sets the color text of the title. It receives a MUI color. Default: 'black'
-//variant (optional): Sets the MUI variant of the text. https://mui.com/api/typography/ for more information. Default: 'h2'
-//imageUrl (optional): The URL of the background image.
-//bckgColor (optional): This will only work if no imageUrl is provided. Sets the background color of the image. Default: #FAFA88 
-//bckgOpacity (optional): Sets the opacity of the background image. Default: '0.7'
-//paddingBlock (optional): Sets the padding-block of the Title component. Default: '6rem'
-
-
 export default function Title({
-  text, 
-  color = 'black',
-  variant = 'h2', 
+  titleText, 
+  titleTextColor = 'black',
+  titleMuiVariant = 'h2', 
   imageUrl, 
   bckgOpacity = '0.7',
   paddingBlock = '6rem'
@@ -30,17 +19,22 @@ export default function Title({
     background: !imageUrl && '#FAFA88', 
   }
 
-
   return (
     <div 
       className={s.container} 
       style={containerStyles}
     >
-      <Typography sx={{fontFamily:"Product Sans"}} variant={variant} color={color}>{text}</Typography>
+      <Typography 
+      sx={{fontFamily:"Product Sans"}} 
+      variant={titleMuiVariant} 
+      color={titleTextColor}>{titleText}</Typography>
       {imageUrl &&
-        <img className={s.bckg} src={imageUrl} style={{opacity: `${bckgOpacity}`}} alt=""/>
+        <img 
+        className={s.bckg} 
+        src={imageUrl} 
+        style={{opacity: `${bckgOpacity}`}} 
+        alt=""/>
       }
-      
     </div>
   )
 }

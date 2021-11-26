7c59755702434bfc317e5d746dedf120184defdf
gdanielgimenez <gdanielgimenez@gmail.com>
2021-11-26 20:43:17 -0300

    form with formik and  material ui layout  added

--- src/Components/Auth/LoginForm.js
+++ src/Components/Auth/LoginForm.js
@@ -1,48 +1,104 @@
 import React, { useState } from 'react';
+import { useFormik } from 'formik';
+import {
+  Alert,
+  AlertTitle,
+  Button,
+  FormControl,
+  Input,
+  InputAdornment,
+  InputLabel,
+} from '@mui/material';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+import VisibilityOutlinedIcon from '@mui/icons-material/VisibilityOutlined';
 import '../FormStyles.css';
 
 const LoginForm = () => {
-  const [initialValues, setInitialValues] = useState({
+  const [userValues, setUserValues] = useState({
     email: '',
     password: '',
   });
+  const SignupForm = () => {
+    const formik = useFormik({
+      initialValues: {
+        email: '',
+        password: '',
+      },
+      //validate,
+      onSubmit: (values) => {
+        setUserValues({
+          ...userValues,
+          email: values.email,
+          password: values.password,
+        });
+        console.log(userValues);
+      },
+    });
 
-  const handleChange = (e) => {
-    if (e.target.name === 'email') {
-      setInitialValues({ ...initialValues, email: e.target.value });
-    }
-    if (e.target.name === 'password') {
-      setInitialValues({ ...initialValues, password: e.target.value });
-    }
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(initialValues);
-    localStorage.setItem('token', 'tokenValueExample');
+    return (
+      <form onSubmit={formik.handleSubmit}>
+        <FormControl margin="dense" sx={{ width: '25rem' }}>
+          <InputLabel htmlFor="email">Email Address</InputLabel>
+          <Input
+            id="email"
+            margin="dense"
+            name="email"
+            placeholder="Email"
+            startAdornment={
+              <InputAdornment position="start">
+                <EmailOutlinedIcon />
+              </InputAdornment>
+            }
+            type="email"
+            value={formik.values.email}
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
+          />
+          {formik.touched.email && formik.errors.email ? (
+            <Alert align="justify" severity="warning" sx={{ width: '23rem' }}>
+              <AlertTitle> Warning </AlertTitle>
+              {formik.errors.email}
+            </Alert>
+          ) : null}
+        </FormControl>
+        <br />
+        <br />
+        <FormControl margin="dense" sx={{ width: '25rem' }}>
+          <InputLabel htmlFor="password">password</InputLabel>
+          <Input
+            id="password"
+            margin="dense"
+            name="password"
+            placeholder="password"
+            startAdornment={
+              <InputAdornment position="start">
+                <VisibilityOutlinedIcon />
+              </InputAdornment>
+            }
+            type="password"
+            value={formik.values.password}
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
+          />
+          {formik.touched.password && formik.errors.password ? (
+            <Alert align="justify" severity="warning" sx={{ width: '23rem' }}>
+              <AlertTitle> Warning </AlertTitle>
+              {formik.errors.password}
+            </Alert>
+          ) : null}
+        </FormControl>
+        <br />
+        <br />
+        <Button type="submit" variant="contained">
+          Login
+        </Button>
+      </form>
+    );
   };
 
   return (
-    <form className="form-container" onSubmit={handleSubmit}>
-      <input
-        className="input-field"
-        name="email"
-        placeholder="Enter email"
-        type="text"
-        value={initialValues.name}
-        onChange={handleChange}
-      />
-      <input
-        className="input-field"
-        name="password"
-        placeholder="Enter password"
-        type="text"
-        value={initialValues.password}
-        onChange={handleChange}
-      />
-      <button className="submit-btn" type="submit">
-        Log In
-      </button>
+    <form className="form-container">
+      <SignupForm />
     </form>
   );
 };

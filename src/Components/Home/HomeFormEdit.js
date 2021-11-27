c1feed71fc3605710a3152afcf919aeabc2a02b9
Gabriel Shimabuku <gaby.shima12@gmail.com>
2021-11-27 14:37:39 -0300

    Added a form which receives a new welcome text and updates the component state on submit. Text is required and must be at least 20 characters long. If submit is successful, it fires an alert

--- src/Components/Home/HomeFormEdit.js
+++ src/Components/Home/HomeFormEdit.js
@@ -1,5 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import Button from '@mui/material/Button';
+import Swal from 'sweetalert2';
 
 export default function HomeFormEdit() {
-  return <div>This is homeformedit</div>;
+  const [welcomeText, setWelcomeText] = useState('');
+
+  return (
+    <>
+      <Formik
+        initialValues={{
+          welcomeText: '',
+        }}
+        validationSchema={Yup.object({
+          welcomeText: Yup.string()
+            .min(
+              20,
+              'El texto de bienvenida debe ser de al menos 20 caracteres',
+            )
+            .required('Este campo es obligatorio'),
+        })}
+        onSubmit={(values, { setSubmitting }) => {
+          Swal.fire({
+            text: 'El texto de bienvenida se actualizó correctamente!',
+            icon: 'success',
+            confirmButtonText: 'Hecho',
+          });
+          setWelcomeText(values.welcomeText);
+          setSubmitting(false);
+        }}>
+        <Form>
+          <label htmlFor="welcomeText"> Texto de bienvenida</label>
+          <Field as="textarea" name="welcomeText" />
+          <ErrorMessage name="welcomeText" />
+
+          <Button color="primary" type="submit" variant="text">
+            Cambiar texto de bienvenida
+          </Button>
+        </Form>
+      </Formik>
+    </>
+  );
 }

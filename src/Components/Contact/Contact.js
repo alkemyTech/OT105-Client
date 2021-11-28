71b87e453460f143dda1bbfa12b6aa5357271d4a
pablo Vieyra <pablin_vieyra1@outlook.com>
2021-11-28 14:20:45 -0300

    file creation and import

---
+++ src/Components/Contact/Contact.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import axios from 'axios';
+import { TextField, Box, Button, Alert, Typography } from '@mui/material';
+import '../FormStyles.css';
+
+const Contact = () => {
+  return <div />;
+};
+
+export default Contact;

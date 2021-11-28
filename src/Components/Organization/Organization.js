aa198ce1665d077f61199b89b2a2821672dd190b
Julio <julio.franco9111@gmail.com>
2021-11-28 13:04:27 -0300

    add name, description, and image props

--- src/Components/Organization/Organization.js
+++ src/Components/Organization/Organization.js
@@ -6,34 +6,26 @@ import Divider from '@mui/material/Divider';
 import { ImageListItem, Typography } from '@mui/material';
 import { Card } from './Card';
 
-export default function Organization() {
+export default function Organization({
+  name = 'Somos más',
+  image = 'https://picsum.photos/800/350',
+  description = '',
+}) {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Card>
         <Typography variant="subtitle1">Name</Typography>
-        <Typography variant="h4">Somos más</Typography>
+        <Typography variant="h4">{name}</Typography>
         <Divider />
         <Typography variant="subtitle1">Image</Typography>
         <ImageListItem>
-          <img
-            alt="imagen-de-ong"
-            fit="cover"
-            load="lazy"
-            src="https://picsum.photos/800/350"
-          />
+          <img alt="imagen-de-ong" fit="cover" load="lazy" src={image} />
         </ImageListItem>
         <Divider />
         <Typography variant="subtitle1">Description</Typography>
         <Typography variant="body1">
-          lorem ipsum dolor sit amet, consectetur adip iscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          {description || 'No se cargó una descripción.'}
         </Typography>
-
         <Divider />
         <br />
         <Link

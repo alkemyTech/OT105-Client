55e05308342b4f19ef250c035b679d15631657ad
josejpm <josejpm@gmail.com>
2021-11-28 13:11:29 -0300

    Custom AppBar updated. dashboard welcome message done

--- src/Components/Backoffice/BackofficeDashboard.js
+++ src/Components/Backoffice/BackofficeDashboard.js
@@ -1,23 +1,43 @@
-import React from 'react';
-import { AppBar, Box, Toolbar, IconButton } from '@mui/material';
-import MenuIcon from '@mui/icons-material/Menu';
+import React, { useState, useEffect } from 'react';
+import { Typography } from '@mui/material';
+import CustomAppBar from './CustomAppBar';
 
 const BackOfficeDashBoard = () => {
+  const [user, setUser] = useState({ name: '', image: '' });
+  const getUser = () => {
+    return {
+      name: 'Jane Doe',
+      image:
+        'https://images.unsplash.com/photo-1573607217032-18299406d100?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=715&q=80',
+    };
+  };
+
+  useEffect(() => {
+    const resp = getUser();
+
+    setUser(resp);
+  }, []);
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar style={{ backgroundColor: '#9AC9FB' }} position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}>
-            <MenuIcon  />
-          </IconButton>
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <>
+      <CustomAppBar user={user} />
+
+      <Typography
+        sx={{ fontSize: 40, textAlign: 'center', marginTop: 20 }}
+        variant="p"
+        gutterBottom
+        component="p">
+        {user.name}
+      </Typography>
+
+      <Typography
+        sx={{ fontSize: 45, textAlign: 'center' }}
+        variant="p"
+        gutterBottom
+        component="p">
+        Bienvenida al dashboard!
+      </Typography>
+    </>
   );
 };
 

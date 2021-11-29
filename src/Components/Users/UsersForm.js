f56b6e1e966cc3283cc699f1c3c223427aeb3e1c
Julio <julio.franco9111@gmail.com>
2021-11-29 00:11:52 -0300

    add Formik to inputs

--- src/Components/Users/UsersForm.js
+++ src/Components/Users/UsersForm.js
@@ -26,59 +26,7 @@ const UserForm = ({ user = null }) => {
         image: '',
       };
 
-  const handleSubmit = async (values, formik) => {
-    const url = 'http://ongapi.alkemy.org/api/';
-
-    try {
-      let response;
-
-      if (user && user.id) {
-        response = await axios.patch(
-          `${url}users/${user.id}`,
-          JSON.stringify({ ...values, role_id: values.role }),
-          {
-            headers: {
-              'Content-Type': 'application/json',
-            },
-          },
-        );
-      } else {
-        response = await axios.post(`${url}users`, JSON.stringify(values), {
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        });
-      }
-
-      if (response.status === 200) {
-        setSuccess({
-          ...success,
-          status: true,
-        });
-        formik.resetForm();
-        clearImage();
-        setTimeout(() => {
-          setSuccess({
-            ...success,
-            status: false,
-          });
-        }, 4000);
-      } else {
-        return new Error('Error');
-      }
-    } catch (e) {
-      setSuccess({
-        status: false,
-        errors: true,
-      });
-      setTimeout(() => {
-        setSuccess({
-          status: false,
-          errors: null,
-        });
-      }, 4000);
-    }
-  };
+  const handleSubmit = async (values, formik) => {};
 
   const setImage = (image) => {
     userValues.image = image;

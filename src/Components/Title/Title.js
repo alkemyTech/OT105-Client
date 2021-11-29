d9fab9bd7e1ece8679ce81b78018d2c6b9be836c
Gabriel Shimabuku <gaby.shima12@gmail.com>
2021-11-29 16:21:16 -0300

    Applied prettier formatter

--- src/Components/Title/Title.js
+++ src/Components/Title/Title.js
@@ -1,43 +1,43 @@
-import React from 'react'
+import React from 'react';
 
 //Styles
-import s from '../../Styles/Title.module.css'
+import s from '../../Styles/Title.module.css';
 //MUI
-import { Typography } from '@mui/material'
+import { Typography } from '@mui/material';
 
 const Title = ({
-  titleText, 
+  titleText,
   titleTextColor = 'black',
-  titleMuiVariant = 'h2', 
-  imageUrl, 
+  titleMuiVariant = 'h2',
+  imageUrl,
   bckgColor = '#FAFA88',
   bckgOpacity = '0.7',
-  titlePadding = '6rem'
+  titlePadding = '6rem',
 }) => {
   const containerStyles = {
-    paddingBlock: titlePadding, 
-    background: !imageUrl && bckgColor, 
-  }
+    paddingBlock: titlePadding,
+    background: !imageUrl && bckgColor,
+  };
 
   return (
-    <div 
-      className={s.container} 
-      style={containerStyles}
-    >
-      <Typography 
-      sx={{fontFamily:"Product Sans"}}
-      align="center"
-      variant={titleMuiVariant} 
-      color={titleTextColor}>{titleText}</Typography>
-      {imageUrl &&
-        <img 
-        className={s.bckg} 
-        src={imageUrl} 
-        style={{opacity: `${bckgOpacity}`}} 
-        alt=""/>
-      }
+    <div className={s.container} style={containerStyles}>
+      <Typography
+        align="center"
+        color={titleTextColor}
+        sx={{ fontFamily: 'Product Sans' }}
+        variant={titleMuiVariant}>
+        {titleText}
+      </Typography>
+      {imageUrl && (
+        <img
+          alt=""
+          className={s.bckg}
+          src={imageUrl}
+          style={{ opacity: `${bckgOpacity}` }}
+        />
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default Title;

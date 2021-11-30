cbd176195ce49f0fab3bd9dfedd69dcd9a93b451
Gabriel Shimabuku <gaby.shima12@gmail.com>
2021-11-30 12:11:42 -0300

    Added success state. If it is true, the component renders the activity details coming from the API. If it is false, it shows a not found message. Removed console.log s

--- src/Components/Activities/Detail/ActivityDetail.js
+++ src/Components/Activities/Detail/ActivityDetail.js
@@ -1,10 +1,8 @@
 import React, { useEffect, useState } from 'react';
 //COMPONENTS
 import Title from '../../Title/Title';
-
 //MUI
 import { CircularProgress, Container, Typography } from '@mui/material';
-
 //In the future this API call logic should be moved.
 import axios from 'axios';
 const getActivityById = (id) => {
@@ -14,18 +12,18 @@ const getActivityById = (id) => {
 export default function ActivityDetail({ match }) {
   const [details, setDetails] = useState({});
   const { name, description, image } = details;
+  const [success, setSuccess] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
-
   const activityId = match.params.id;
 
   useEffect(() => {
     getActivityById(activityId)
       .then((res) => {
-        console.log(res.data);
+        setSuccess(true);
         setDetails(res.data.data);
       })
       .catch((e) => {
-        console.log(e.response.data);
+        setSuccess(false);
         setDetails(e.response.data);
       })
       .finally(() => setIsLoading(false));
@@ -46,21 +44,34 @@ export default function ActivityDetail({ match }) {
     );
   } else
     return (
-      <div>
-        <Title
-          bckgOpacity="0.5"
-          imageUrl={image}
-          titlePadding="10rem"
-          titleText={name}
-        />
-        <Container maxWidth="md" sx={{ marginTop: '3rem' }}>
-          <Typography color="initial" sx={{ marginBlock: '1rem' }} variant="h4">
-            Detalle de actividad
-          </Typography>
-          <Typography color="initial" variant="body1">
-            {description}
-          </Typography>
-        </Container>
-      </div>
+      <>
+        {success ? (
+          <div>
+            <Title
+              bckgOpacity="0.5"
+              imageUrl={image}
+              titlePadding="10rem"
+              titleText={name}
+            />
+            <Container maxWidth="md" sx={{ marginTop: '3rem' }}>
+              <Typography
+                color="initial"
+                sx={{ marginBlock: '1rem' }}
+                variant="h4">
+                Detalle de actividad
+              </Typography>
+              <Typography color="initial" variant="body1">
+                {description}
+              </Typography>
+            </Container>
+          </div>
+        ) : (
+          <Title
+            titlePadding="12rem"
+            titleText="Actividad no encontrada"
+            variant="h3"
+          />
+        )}
+      </>
     );
 }

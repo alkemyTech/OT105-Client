f0db05deeed39312c0f1b828a26899dba3454196
Julio <julio.franco9111@gmail.com>
2021-11-29 22:48:14 -0300

    add validator

--- src/Components/Users/UsersForm.js
+++ src/Components/Users/UsersForm.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { Field, Form, Formik } from 'formik';
 import '../FormStyles.css';
+import { Field, Form, Formik } from 'formik';
+import { Alert } from '@mui/material';
+import { UserFormSchema } from './UserFormSchema';
 
 const UserForm = ({ user = null }) => {
   const userValues = user
@@ -21,43 +23,66 @@ const UserForm = ({ user = null }) => {
     <Formik
       className="form-container"
       initialValues={userValues}
+      validationSchema={UserFormSchema}
       onSubmit={handleSubmit}>
-      <Form className="form-container" onSubmit={handleSubmit}>
-        <h1>{user ? 'Edit User' : 'Create User'}</h1>
-        <label htmlFor="userName">User name</label>
-        <Field
-          className="input-field"
-          name="name"
-          placeholder="User name"
-          type="text"
-          value={userValues.name}
-        />
-        <label htmlFor="userEmail">User email</label>
-        <Field
-          className="input-field"
-          name="email"
-          placeholder="User email"
-          type="text"
-          value={userValues.email}
-        />
-        <label htmlFor="userEmail">User role</label>
-        <Field
-          as="select"
-          className="input-field"
-          name="role"
-          placeholder="User email"
-          type="text"
-          value={userValues.role}>
-          <option disabled value="">
-            Select the role
-          </option>
-          <option value="1">Admin</option>
-          <option value="2">User</option>
-        </Field>
-        <button className="submit-btn" type="submit">
-          {user ? 'Edit User' : 'Create User'}
-        </button>
-      </Form>
+      {({ errors, touched }) => (
+        <Form className="form-container">
+          <h1>{user ? 'Edit User' : 'Create User'}</h1>
+          <label htmlFor="userName">User name</label>
+          <Field
+            className="input-field"
+            name="name"
+            placeholder="User name"
+            type="text"
+            value={userValues.name}
+          />
+          {errors.name && touched.name ? (
+            <Alert severity="warning">{errors.name}</Alert>
+          ) : null}
+
+          <label htmlFor="userEmail">User email</label>
+          <Field
+            className="input-field"
+            name="email"
+            placeholder="User email"
+            type="text"
+            value={userValues.email}
+          />
+          {errors.email && touched.email ? (
+            <Alert severity="warning">{errors.email}</Alert>
+          ) : null}
+
+          <label htmlFor="userEmail">User role</label>
+          <Field
+            as="select"
+            className="input-field"
+            name="role"
+            placeholder="User email"
+            type="text"
+            value={userValues.role}>
+            <option disabled value="">
+              Select the role
+            </option>
+            <option value="1">Admin</option>
+            <option value="2">User</option>
+          </Field>
+          {errors.role && touched.role ? (
+            <Alert severity="warning">{errors.role}</Alert>
+          ) : null}
+
+          <label htmlFor="userEmail">User password</label>
+          <Field
+            className="input-field"
+            name="password"
+            placeholder="User password"
+            type="text"
+            value={userValues.passsword}
+          />
+          <button className="submit-btn" type="submit">
+            {user ? 'Edit User' : 'Create User'}
+          </button>
+        </Form>
+      )}
     </Formik>
   );
 };

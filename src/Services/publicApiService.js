51c46b69d1891a1e4018b9805a306034f5fe971e
Gabriel Shimabuku <gaby.shima12@gmail.com>
2021-12-01 15:53:45 -0300

    Created async method publicApiGet, which receives 2 parameters: a route (required) and an id (optional). If id is provided, it will request one specific element. Else, it will request an array of elements.

--- src/Services/publicApiService.js
+++ src/Services/publicApiService.js
@@ -2,15 +2,30 @@ import axios from 'axios';
 
 const config = {
   headers: {
-    Group: 1, //Aqui va el ID del equipo!!
+    Group: 105, //Aqui va el ID del equipo!!
   },
 };
 
-const Get = () => {
-  axios
-    .get('https://jsonplaceholder.typicode.com/users', config)
-    .then((res) => console.log(res))
-    .catch((err) => console.log(err));
+const publicApiGet = async (route, id) => {
+  let url;
+
+  if (id === undefined || id === null) {
+    url = `http://ongapi.alkemy.org/api${route}`;
+  } else {
+    if (typeof id !== 'number') {
+      throw 'id must be type Number';
+    } else {
+      url = `http://ongapi.alkemy.org/api${route}/${id}`;
+    }
+  }
+
+  try {
+    const res = await axios.get(url, config);
+
+    return res.data.data;
+  } catch (err) {
+    return err.response.data;
+  }
 };
 
-export default Get;
+export { publicApiGet };

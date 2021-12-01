28427f5270b2fb7456773e821915ef24984b3ace
josejpm <josejpm@gmail.com>
2021-11-30 23:02:25 -0300

    Styles added, Dynamic data loading done, MUI components added

--- src/Components/News/Detail/NewsDetail.js
+++ src/Components/News/Detail/NewsDetail.js
@@ -1,22 +1,47 @@
-import React from 'react';
-import { Typography } from '@mui/material';
+import React, { useState, useEffect } from 'react';
+
+import { Typography, Box } from '@mui/material';
 import Title from '../../Title/Title';
+import style from '../../../Styles/NewsDetail/NewsDetail.module.css';
 
 const NewsDetail = () => {
+  const [newsDetail, setNewsDetail] = useState({
+    name: '',
+    content: '',
+    image: '',
+  });
+
+  const getNewsDetails = () => ({
+    name: 'Titulo de la noticia',
+    content:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus omnis dolor illum error quas iusto eum, rerum, ad at quis perferendis fuga. Dolorem repellat, quae qui quisquam perspiciatis numquam nesciunt!. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus omnis dolor illum error quas iusto eum, rerum, ad at quis perferendis fuga. Dolorem repellat, quae qui quisquam perspiciatis numquam nesciunt!',
+    image:
+      'https://images.unsplash.com/photo-1592060036126-1b6d5139dea4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+  });
+
+  const updateCurrentNews = () => {
+    const currentNews = getNewsDetails();
+
+    setNewsDetail(currentNews);
+  };
+
+  useEffect(() => {
+    updateCurrentNews();
+  }, []);
+
   return (
     <div>
-      <Title />
-      <div>
-        <div>Image Here</div>
-        <div>
-          <Typography variant="h4" component="h3">
-            News Title Here
-          </Typography>
+      <Title titleText={newsDetail.name} imageUrl={newsDetail.image} />
+      <Box component="div" className={style.container}>
+        <Box component="div" className={style.content}>
           <Typography variant="p" component="p">
-            News Content Here
+            {newsDetail.content}
           </Typography>
-        </div>
-      </div>
+        </Box>
+        <Box component="div" className={style.image}>
+          <img src={newsDetail.image} alt="childrens at play" />
+        </Box>
+      </Box>
     </div>
   );
 };

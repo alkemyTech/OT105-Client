b96426f1b28333970002b532bee4defac4caabbd
gdanielgimenez <gdanielgimenez@gmail.com>
2021-12-02 17:26:48 -0300

    form with formik and a file with the  validations added and minor css for the form

--- src/Components/EditForm/EditFormOrg.js
+++ src/Components/EditForm/EditFormOrg.js
@@ -1,8 +1,245 @@
 import React, { useState } from 'react';
+import { useFormik } from 'formik';
+import { useDropzone } from 'react-dropzone';
+import {
+  Alert,
+  AlertTitle,
+  Button,
+  Container,
+  FormControl,
+  Input,
+  InputLabel,
+  TextField,
+} from '@mui/material';
+import validate from './validate';
+import Collapse from '@mui/material/Collapse';
+import IconButton from '@mui/material/IconButton';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import CloseIcon from '@mui/icons-material/Close';
 function EditFormOrg() {
+  const [open, setOpen] = useState(true);
+
+  const Edit = () => {
+    const { acceptedFiles, getRootProps, getInputProps } = useDropzone({
+      accept: 'image/jpeg, image/png',
+      onDrop: (acceptedFiles) => {
+        console.log(acceptedFiles);
+      },
+    });
+    const files = acceptedFiles.map((file) => (
+      <li key={file.path}>
+        {file.path} - {file.size} bytes
+      </li>
+    ));
+    const handleChange = (e, editor) => {
+      const data = editor.getData();
+
+      formik.setFieldValue('shortDescription', data);
+    };
+
+    const formik = useFormik({
+      initialValues: {
+        name: '',
+        logo: '',
+        shortDescription: '',
+        longDescription: '',
+        link1: '',
+        link2: '',
+        link3: '',
+      },
+      validate,
+      onSubmit: (values) => {},
+    });
+
+    return (
+      <form className="formEditOrg" onSubmit={formik.handleSubmit}>
+        <FormControl margin="dense">
+          <InputLabel htmlFor="name">Company name</InputLabel>
+          <Input
+            id="name"
+            margin="dense"
+            name="name"
+            sx={{ width: '25rem' }}
+            type="text"
+            value={formik.values.name}
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
+          />
+          {formik.touched.name && formik.errors.name ? (
+            <Alert
+              align="justify"
+              severity="warning"
+              sx={{ width: '23rem', height: '2rem' }}>
+              <AlertTitle>{formik.errors.name}</AlertTitle>
+            </Alert>
+          ) : null}
+        </FormControl>
+        <br />
+        <br />
+        <InputLabel>Logo</InputLabel>
+        <FormControl>
+          <div {...getRootProps()}>
+            <input
+              id="logo"
+              name="logo"
+              value={formik.values.logo}
+              onChange={formik.handleChange}
+              {...getInputProps()}
+              onBlur={formik.handleBlur}
+            />
+            <p>Drag and drop some files here, or click to select files</p>
+          </div>
+          <aside>
+            <h4>Files</h4>
+            <ul>{files}</ul>
+          </aside>
+          {formik.errors.logo && formik.touched.logo ? (
+            <Collapse in={open}>
+              <Alert
+                action={
+                  <IconButton
+                    aria-label="close"
+                    color="inherit"
+                    size="small"
+                    onClick={() => {
+                      setOpen(false);
+                    }}>
+                    <CloseIcon fontSize="inherit" />
+                  </IconButton>
+                }
+                align="justify"
+                severity="warning"
+                sx={{ width: '23rem' }}>
+                <AlertTitle>Warning</AlertTitle>
+                {formik.errors.logo}
+              </Alert>
+            </Collapse>
+          ) : null}
+        </FormControl>
+        <br />
+        <br />
+        <Container sx={{ width: '25rem' }}>
+          <CKEditor
+            required
+            data={formik.values.shortDescription}
+            editor={ClassicEditor}
+            id="shortDescription"
+            name="shortDescription"
+            type="text"
+            onChange={handleChange}
+          />
+          {formik.touched.shortDescription && formik.errors.shortDescription ? (
+            <Alert
+              align="justify"
+              severity="warning"
+              sx={{ width: '23rem', height: '2rem' }}>
+              <AlertTitle> {formik.errors.shortDescription}</AlertTitle>
+            </Alert>
+          ) : null}
+        </Container>
+        <br />
+        <FormControl margin="dense">
+          <TextField
+            multiline
+            id="longDescription"
+            name="longDescription"
+            placeholder="Long description"
+            rows={4}
+            sx={{ width: '25rem' }}
+            type="text"
+            value={formik.values.longDescription}
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
+          />
+          {formik.touched.longDescription && formik.errors.longDescription ? (
+            <Alert
+              align="justify"
+              severity="warning"
+              sx={{ width: '23rem', height: '2rem' }}>
+              <AlertTitle>{formik.errors.longDescription}</AlertTitle>
+            </Alert>
+          ) : null}
+        </FormControl>
+        <br />
+        <InputLabel> social media links</InputLabel>
+        <FormControl margin="dense">
+          <TextField
+            id="link1"
+            margin="dense"
+            name="link1"
+            placeholder="link1"
+            size="small"
+            sx={{ width: '25rem' }}
+            type="url"
+            value={formik.values.link1}
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
+          />
+          {formik.touched.link1 && formik.errors.link1 ? (
+            <Alert
+              align="justify"
+              severity="warning"
+              sx={{ width: '23rem', height: '2rem' }}>
+              <AlertTitle>{formik.errors.link1}</AlertTitle>
+            </Alert>
+          ) : null}
+          <TextField
+            id="link2"
+            margin="dense"
+            name="link2"
+            placeholder="link2"
+            size="small"
+            sx={{ width: '25rem' }}
+            type="url"
+            value={formik.values.link2}
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
+          />
+          {formik.touched.link2 && formik.errors.link2 ? (
+            <Alert
+              align="justify"
+              severity="warning"
+              sx={{ width: '23rem', height: '2rem' }}>
+              <AlertTitle> {formik.errors.link2}</AlertTitle>
+            </Alert>
+          ) : null}
+          <TextField
+            id="link3"
+            margin="dense"
+            name="link3"
+            placeholder="link3"
+            size="small"
+            sx={{ width: '25rem' }}
+            type="url"
+            value={formik.values.link3}
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
+          />
+          {formik.touched.link3 && formik.errors.link3 ? (
+            <Alert
+              align="justify"
+              severity="warning"
+              sx={{ width: '23rem', height: '2rem' }}>
+              <AlertTitle> {formik.errors.link3}</AlertTitle>
+            </Alert>
+          ) : null}
+        </FormControl>
+
+        <br />
+        <br />
+        <br />
+        <Button className="btn" type="submit" variant="contained">
+          {' '}
+          submit
+        </Button>
+      </form>
+    );
+  };
+
   return (
     <div>
-      <h1>Form</h1>
+      <Edit />
     </div>
   );
 }

2e165c3ed5051518373030601e70204a71216ebd
AlVARO BARREIRA <alvarobarreiradev@gmail.com>
2021-12-02 16:48:17 -0300

    Activities route added and unnecessary code eliminated

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import SchoolCampaign from './Campaigns/School/SchoolCampaign';
 import ToysCampaign from './Campaigns/Toys/ToysCampaign';
 import MembersForm from './Components/Members/MembersForm';
 import ProjectsForm from './Components/Projects/ProjectsForm';
+import Activities from './Components/Activities/Activities';
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
         <Switch>
           {/* <Route path="/" exact component={} />           Esta ruta debe ser para el Home */}
           <Route component={ActivitiesForm} path="/create-activity" />
+          <Route component={Activities} path="/Activities" />
           <Route component={CategoriesForm} path="/create-category" />
           <Route component={NewsForm} path="/create-news" />
           <Route component={SlidesForm} path="/backoffice/create-slide" />
@@ -32,49 +34,6 @@ function App() {
           <Route component={ToysCampaign} path="/toys-campaign" />
         </Switch>
       </BrowserRouter>
-      <div className="App">
-        <header className="App-header">
-          <img alt="logo" className="App-logo" src={logo} />
-          <Counter />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <span>
-            <span>Learn </span>
-            <a
-              className="App-link"
-              href="https://reactjs.org/"
-              rel="noopener noreferrer"
-              target="_blank">
-              React
-            </a>
-            <span>, </span>
-            <a
-              className="App-link"
-              href="https://redux.js.org/"
-              rel="noopener noreferrer"
-              target="_blank">
-              Redux
-            </a>
-            <span>, </span>
-            <a
-              className="App-link"
-              href="https://redux-toolkit.js.org/"
-              rel="noopener noreferrer"
-              target="_blank">
-              Redux Toolkit
-            </a>
-            ,<span> and </span>
-            <a
-              className="App-link"
-              href="https://react-redux.js.org/"
-              rel="noopener noreferrer"
-              target="_blank">
-              React Redux
-            </a>
-          </span>
-        </header>
-      </div>
     </>
   );
 }

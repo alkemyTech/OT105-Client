d4272321ff73744ab07d3adbd8cb5523afdf9a61
Josejpm <josejpm@gmail.com>
2021-12-02 13:18:02 -0300

    contact route added to App,js, ContactCard component created, Contact component created

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import ToysCampaign from './Campaigns/Toys/ToysCampaign';
 import MembersForm from './Components/Members/MembersForm';
 import ProjectsForm from './Components/Projects/ProjectsForm';
 import Activities from './Components/Activities/Activities';
+import Contact from './Components/Contact/Contact';
 
 function App() {
   return (
@@ -32,6 +33,7 @@ function App() {
           <Route component={ProjectsForm} path="/create-project" />
           <Route component={SchoolCampaign} path="/school-campaign" />
           <Route component={ToysCampaign} path="/toys-campaign" />
+          <Route component={Contact} path="/contact" />
         </Switch>
       </BrowserRouter>
     </>

09493a035d58af39a5dce20b154e0f6693a4c6b9
Julio <julio.franco9111@gmail.com>
2021-12-04 20:48:13 -0300

    remove counter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import ActivitiesForm from './Components/Activities/ActivitiesForm';
@@ -32,49 +30,6 @@ function App() {
           <Route component={ToysCampaign} path="/toys-campaign" />
         </Switch>
       </BrowserRouter>
-      <div className="App">
-        <header className="App-header">
-          <img alt="logo" className="App-logo" src={logo} />
-          <Counter />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <span>
-            <span>Learn </span>
-            <a
-              className="App-link"
-              href="https://reactjs.org/"
-              rel="noopener noreferrer"
-              target="_blank">
-              React
-            </a>
-            <span>, </span>
-            <a
-              className="App-link"
-              href="https://redux.js.org/"
-              rel="noopener noreferrer"
-              target="_blank">
-              Redux
-            </a>
-            <span>, </span>
-            <a
-              className="App-link"
-              href="https://redux-toolkit.js.org/"
-              rel="noopener noreferrer"
-              target="_blank">
-              Redux Toolkit
-            </a>
-            ,<span> and </span>
-            <a
-              className="App-link"
-              href="https://react-redux.js.org/"
-              rel="noopener noreferrer"
-              target="_blank">
-              React Redux
-            </a>
-          </span>
-        </header>
-      </div>
     </>
   );
 }

f63d796bf3674a815e5f789a6ecda8dda4787ec8
Gabriel Shimabuku <gaby.shima12@gmail.com>
2021-12-05 09:14:49 -0300

    Added mercadopago_logo.png & foto11.jpg to public/images folder. Donations component now shows a background image, some text and a mercadopago button, which does nothing yet. Added Donations.module.css in Styles folder

--- src/Components/Donations/Donations.js
+++ src/Components/Donations/Donations.js
@@ -1,7 +1,42 @@
 import React from 'react';
+import s from '../../Styles/Donations.module.css';
+import { Card, CardActionArea, CardContent, Typography } from '@mui/material/';
+const Donations = ({ donationsTitle, donationsSubtitle }) => {
+  return (
+    <div className={s.container}>
+      <div className={s.donationsText}>
+        <Typography
+          align="center"
+          className={s.donationsTitle}
+          color="white"
+          variant="h1">
+          Donaciones
+        </Typography>
+        <Typography
+          align="center"
+          className={s.donationsSubtitle}
+          color="white"
+          variant="h3">
+          Ayúdanos a crecer!
+        </Typography>
+      </div>
+      <Card
+        className={s.card}
+        sx={{ width: '14rem', margin: '3rem', borderRadius: '100px' }}>
+        <CardActionArea>
+          <CardContent>
+            <img
+              alt="mercadopago logo"
+              className={s.logo}
+              src="/images/mercadopago_logo.png"
+            />
+          </CardContent>
+        </CardActionArea>
+      </Card>
 
-const Donations = () => {
-  return <div>This is donations page</div>;
+      <img alt="" className={s.image} src="/images/foto11.jpg" />
+    </div>
+  );
 };
 
 export default Donations;

363344ecfb6a5b8e63f86d647a80044e45ed99b5
pablo Vieyra <pablin_vieyra1@outlook.com>
2021-12-06 19:31:41 -0300

    corrections

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState } from 'react';
 import { useFormik } from 'formik';
 import { emailRegex } from '../../utils';
 import { TextField, Box, Button, Alert, Typography } from '@mui/material';

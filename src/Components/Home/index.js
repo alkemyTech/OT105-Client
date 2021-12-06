8cee931f8b15dafd264b85fd278ba028183b25ec
Julio <julio.franco9111@gmail.com>
2021-12-06 02:18:56 -0300

    add placeholder image for slider and ultimas novedades

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import { Typography } from '@mui/material';
 
 const HomeScreen = () => {
   return (
@@ -8,13 +10,20 @@ const HomeScreen = () => {
         width: '99.9%',
         textAlign: 'center',
         height: '100vh',
-        border: '1px solid',
         overflow: 'hidden',
       }}>
-      <div style={{ objectFit: 'fill' }}>
-        <img src="https://picsum.photos/2500/400" />
+      <div>
+        <img fit="cover" src="https://picsum.photos/3200/700" width="100%" />
       </div>
-      <h1>Texto de bienvenida</h1>
+      <Container sx={{ marginTop: '20px' }}>
+        <Typography variant="h3">Texto de bienvenida</Typography>
+      </Container>
+      <Container sx={{ border: '1px solid', marginTop: '50px' }}>
+        <Box sx={{ objectFit: 'fill' }}>
+          <Typography variant="h4">Últimas novedades</Typography>
+          <img src="https://picsum.photos/1500/500" width="100%" />
+        </Box>
+      </Container>
     </Box>
   );
 };

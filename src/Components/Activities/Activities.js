6c7c9a5de17c38cb0268242bf9714ac762268a13
AlVARO BARREIRA <alvarobarreiradev@gmail.com>
2021-12-07 11:58:38 -0300

    Activities List component added

--- src/Components/Activities/Activities.js
+++ src/Components/Activities/Activities.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Title from '../Title/Title';
+import ActivitiesList from './ActivitiesList';
 
 function Activities() {
   return (
     <div>
       <Title titleText={'Actividades'} />
+      <ActivitiesList />
     </div>
   );
 }

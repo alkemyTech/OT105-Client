60d47875c297a54513951885d747724952e81d51
Gabriel Shimabuku <gaby.shima12@gmail.com>
2021-12-07 15:40:19 -0300

    Created ImageLazyLoader component in src/News folder. It receives 3 props: src (required, the image source), reservedHeight (optional, this helps to avoid loading images simultaneously when they are stacked) and observerOptions (optional, sets the options of the intersection observer)

---
+++ src/Components/News/ImageLazyLoader.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState, useRef } from 'react';
+
+export const ImageLazyLoader = ({
+  src,
+  reservedHeight = '0px',
+  observerOptions = { rootMargin: '0px', threshold: 0.0 },
+}) => {
+  const [visible, setIsVisible] = useState(false);
+  const containerRef = useRef();
+
+  useEffect(() => {
+    const onChange = (entries) => {
+      const el = entries[0];
+
+      if (el.isIntersecting) {
+        setIsVisible(true);
+        observer.disconnect();
+      }
+    };
+    const observer = new IntersectionObserver(onChange, observerOptions);
+
+    observer.observe(containerRef.current);
+
+    return () => observer.disconnect();
+  }, []);
+
+  return (
+    <div
+      ref={containerRef}
+      style={{
+        minHeight: reservedHeight,
+      }}>
+      {visible ? <img alt="" src={src} width="320" /> : null}
+    </div>
+  );
+};

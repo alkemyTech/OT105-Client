ab10db5d9d26b4e9bf80e8d39509014ddb99b4b9
AlVARO BARREIRA <alvarobarreiradev@gmail.com>
2021-12-08 17:52:41 -0300

    Corrections functions

--- src/Services/activitiesService.js
+++ src/Services/activitiesService.js
@@ -1,24 +1,24 @@
 import Axios from 'axios';
 import Swal from 'sweetalert2';
 
-const baseUrl = 'http://ongapi.alkemy.org/api/categories/';
+const baseUrl = 'http://ongapi.alkemy.org/api/activities';
 
 export const createOrUpdateActivity = (id, data) => {
-  if (id) {
-    createActivity(id, data);
+  if (!id) {
+    createActivity(data);
   } else {
-    updateActivity(data);
+    updateActivity(id, data);
   }
 };
 
-const createActivity = async (id, data) => {
-  await Axios.patch(`${baseUrl}${id}`, data).then(() => {
-    Swal.fire('success');
-  });
+const createActivity = (data) => {
+  Axios.post(baseUrl, data);
+  Swal.fire('success');
+  console.log(data);
 };
 
-const updateActivity = async (data) => {
-  await Axios.post(`${baseUrl}`, data).then(() => {
+const updateActivity = async (id, data) => {
+  await Axios.patch(`${baseUrl}/${id}`, data).then(() => {
     console.log(data);
     Swal.fire('success');
   });

deb773f0cec9b98ae85253528ff18efdaea26955
AlVARO BARREIRA <alvarobarreiradev@gmail.com>
2021-12-08 15:11:21 -0300

    Reusable POST and GET methods added

---
+++ src/Services/contactsService.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const CONTACTS_URL = 'http://ongapi.alkemy.org/api/contacts';
+
+const getContactInfo = async () => {
+  try {
+    const res = await axios.get(CONTACTS_URL);
+
+    return console.log(res.data);
+  } catch (err) {
+    return err.response.data;
+  }
+};
+
+const sendContactData = async (data) => {
+  try {
+    const res = await axios.post(CONTACTS_URL, data);
+
+    return console.log(res.data);
+  } catch (err) {
+    return err.response.data;
+  }
+};
+
+export { sendContactData, getContactInfo };

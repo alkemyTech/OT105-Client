f166491179c240a0e607f2696e5dd18a10b5d340
pdvallejos <82845564+pdvallejos@users.noreply.github.com>
2021-12-11 14:56:21 -0300

    se modifica privateApiService agregando metodo post reutilizable

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -17,8 +17,19 @@ const config = {
 const Get = () => {
   axios
     .get('https://jsonplaceholder.typicode.com/users', config)
+    // eslint-disable-next-line
     .then((res) => console.log(res))
+    // eslint-disable-next-line
     .catch((err) => console.log(err));
+};
+
+export const privatePost = async (url, data) => {
+  return await axios
+    .post(url, data, config)
+    // eslint-disable-next-line
+    .then((response) => console.log(response))
+    // eslint-disable-next-line
+    .catch((error) => console.log(error));
 };
 
 export default Get;

e426086d703a87f1fb89a86a0b2ef146d93336b3
josejpm <josejpm@gmail.com>
2021-12-12 19:21:06 -0300

    organizationService errors fixed

--- src/Services/organizationService.js
+++ src/Services/organizationService.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 const getOrganizacion = async () => {
   try {
     const response = await axios(process.env.REACT_APP_GET_ORGANIZATION);
+
     return response.data;
   } catch (err) {
     return err.respose.data;
@@ -14,25 +15,30 @@ const getOrganizacionById = async (id) => {
     const response = await axios(
       `${process.env.REACT_APP_GET_ORGANIZATION_BY_ID}/${id}`,
     );
+
     return response.data;
   } catch (err) {
     return err.respose.data;
   }
 };
 
-const editOrganization = (id, body) => {
+const editOrganization = async (id, body) => {
   try {
-    const response = axios.put(
+    const response = await axios.put(
       `${process.env.REACT_APP_EDIT_ORGANIZATION}/${id}`,
       body,
     );
+
     return response.data;
-  } catch (err) {}
+  } catch (err) {
+    return err.response.data;
+  }
 };
 
-const createOrganization = (body) => {
+const createOrganization = async (body) => {
   try {
-    const response = await axios.post(process.env.CREATE_ORGANIZATION);
+    const response = await axios.post(process.env.CREATE_ORGANIZATION, body);
+
     return response.data;
   } catch (err) {
     return err.response.data;

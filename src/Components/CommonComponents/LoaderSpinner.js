e99e3275258d8320e5c6a1f83a321b3075f5f248
Julio <julio.franco9111@gmail.com>
2021-12-13 17:14:25 -0300

    separate styles in constants

--- src/Components/CommonComponents/LoaderSpinner.js
+++ src/Components/CommonComponents/LoaderSpinner.js
@@ -3,41 +3,35 @@ import { Box } from '@mui/material';
 import Loader from 'react-loader-spinner';
 import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 
-// if recives props.full shows a fullscreen loader component
 const LoaderSpinner = ({ full = false }) => {
-  if (full) {
-    return (
-      <Box
-        sx={{
-          height: '100vh',
-          width: '100%',
-          position: 'absolute',
-          backgroundColor: 'rgba(255,255,255,0.99)',
-          zIndex: '3',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}>
-        <Loader color="#9AC9FB" height={100} type="ThreeDots" width={100} />
-      </Box>
-    );
-  } else {
-    return (
-      <Box
-        sx={{
-          height: '100%',
-          width: '100%',
-          position: 'relative',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}>
-        <Loader color="#9AC9FB" height={100} type="ThreeDots" width={100} />
-      </Box>
-    );
-  }
+  const flexStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  };
+  const styleFullBox = {
+    height: '100vh',
+    width: '100%',
+    position: 'absolute',
+    backgroundColor: 'rgba(255,255,255,0.99)',
+    zIndex: '3',
+  };
+
+  const styleBox = {
+    height: '100%',
+    width: '100%',
+    position: 'relative',
+  };
+
+  return (
+    <Box
+      sx={
+        full ? { ...flexStyle, ...styleFullBox } : { ...flexStyle, ...styleBox }
+      }>
+      <Loader color="#9AC9FB" height={100} type="ThreeDots" width={100} />
+    </Box>
+  );
 };
 
 export default LoaderSpinner;

61d71ee1d22b3b5e927790aeb8c0c39fd3c444d2
AlVARO BARREIRA <alvarobarreiradev@gmail.com>
2021-12-13 22:16:35 -0300

    Correction on icon butons, respective action functions added

--- src/Components/Slides/SlidesBackOffice.js
+++ src/Components/Slides/SlidesBackOffice.js
@@ -1,8 +1,7 @@
-import React from 'react';
-import { styled } from '@mui/material/styles';
+import React, { useState } from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
-import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import { IconButton } from '@mui/material';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
@@ -12,38 +11,44 @@ import EditIcon from '@mui/icons-material/Edit';
 import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
 import { Link } from 'react-router-dom';
 import '../../Styles/SlidesBackOffice.css';
-
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
-  [`&.${tableCellClasses.head}`]: {
-    backgroundColor: theme.palette.common.black,
-    color: theme.palette.common.white,
-  },
-  [`&.${tableCellClasses.body}`]: {
-    fontSize: 14,
-  },
-}));
-
-const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  '&:nth-of-type(odd)': {
-    backgroundColor: theme.palette.action.hover,
-  },
-  '&:last-child td, &:last-child th': {
-    border: 0,
-  },
-}));
-
-function createData(title, image, order, edit, delet) {
-  return { title, image, order, edit, delet };
-}
-
-const rows = [
-  createData('Title 1', 'image 1', 'Order 1', <EditIcon />, <DeleteIcon />),
-  createData('Title 2', 'image 2', 'Order 2', <EditIcon />, <DeleteIcon />),
-  createData('Title 3', 'image 3', 'Order 3', <EditIcon />, <DeleteIcon />),
-  createData('Title 4', 'image 4', 'Order 4', <EditIcon />, <DeleteIcon />),
-];
+import {
+  StyledTableCell,
+  StyledTableRow,
+} from '../../utils/SlidesBackOfficeStyled';
 
 function SlidesBackOffice() {
+  const [mockedData, setMockedData] = useState([
+    {
+      id: 1,
+      Title: 'Title 1',
+      image: 'Image 1',
+      order: 'Order 1',
+    },
+    {
+      id: 2,
+      Title: 'Title 2',
+      image: 'Image 2',
+      order: 'Order 2',
+    },
+    {
+      id: 3,
+      Title: 'Title 3',
+      image: 'Image 3',
+      order: 'Order 3',
+    },
+    {
+      id: 4,
+      Title: 'Title 4',
+      image: 'Image 4',
+      order: 'Order 4',
+    },
+  ]);
+  const deleteSlide = (row) => {
+    const filterArray = mockedData.filter((slide) => slide.id !== row.id);
+
+    return setMockedData(filterArray);
+  };
+
   return (
     <div style={{ height: 400, width: '900px', margin: '20px auto' }}>
       <Link exact className="link-button" to="/backoffice/Slides/create">
@@ -62,15 +67,27 @@ function SlidesBackOffice() {
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows.map((row) => (
-              <StyledTableRow key={row.title}>
+            {mockedData.map((row) => (
+              <StyledTableRow key={row.id}>
                 <StyledTableCell component="th" scope="row">
-                  {row.title}
+                  {row.Title}
                 </StyledTableCell>
                 <StyledTableCell align="right">{row.image}</StyledTableCell>
                 <StyledTableCell align="right">{row.order}</StyledTableCell>
-                <StyledTableCell align="right">{row.edit}</StyledTableCell>
-                <StyledTableCell align="right">{row.delet}</StyledTableCell>
+                <StyledTableCell align="right">
+                  <IconButton
+                    component={Link}
+                    to={`/backoffice/Slides/edit/${row.id}`}
+                    variant="outlined">
+                    <EditIcon />
+                  </IconButton>
+                </StyledTableCell>
+                <StyledTableCell align="right">
+                  <IconButton onClick={() => deleteSlide(row)}>
+                    {' '}
+                    <DeleteIcon />
+                  </IconButton>
+                </StyledTableCell>
               </StyledTableRow>
             ))}
           </TableBody>

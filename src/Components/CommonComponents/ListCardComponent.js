44be32616f9de87fde831229624d1c4ac870f5be
Julio <julio.franco9111@gmail.com>
2021-12-14 10:05:01 -0300

    remove actions, add list as props and remove unused useEffect

--- src/Components/CommonComponents/ListCardComponent.js
+++ src/Components/CommonComponents/ListCardComponent.js
@@ -1,19 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import { Box, Container } from '@mui/material';
 
-const ListCardComponent = () => {
-  const [listData, setListData] = useState();
-
-  useEffect(() => {
-    //method for fetch data and update state
-    //setListData(data);
-  }, []);
+const ListCardComponent = ({ list }) => {
+  const [listData, setListData] = useState(list);
 
   const placeholderImage =
     'https://www.palomacornejo.com/wp-content/uploads/2021/08/no-image.jpg';
@@ -36,25 +29,22 @@ const ListCardComponent = () => {
           margin: '0 auto',
         }}>
         {listData &&
-          listData.map((newData) => (
-            <Card key={newData.id} sx={{ minWidth: 300, maxWidth: 300 }}>
+          listData.map((item) => (
+            <Card key={item.id} sx={{ minWidth: 300, maxWidth: 300 }}>
               <CardMedia
-                alt={newData.title + ' image'}
+                alt={item.title + ' image'}
                 component="img"
                 height="140"
-                image={newData.image || placeholderImage}
+                image={item.image || placeholderImage}
               />
               <CardContent>
                 <Typography gutterBottom component="div" variant="h5">
-                  {newData.title}
+                  {item.title}
                 </Typography>
                 <Typography color="text.secondary" variant="body2">
-                  {newData.description}
+                  {item.description}
                 </Typography>
               </CardContent>
-              <CardActions>
-                <Button size="small">Saber más</Button>
-              </CardActions>
             </Card>
           ))}
       </Container>

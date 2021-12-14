3cf0a376945b796ff45c855fb3d3d90abe3d845e
Julio <julio.franco9111@gmail.com>
2021-12-14 10:23:55 -0300

    remove images, comments, add const mockup for title and use reusable title component

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,38 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import { Typography } from '@mui/material';
+import Title from '../Title/Title';
 
 const HomeScreen = () => {
+  const [title, setTitle] = useState(null);
+  const titleMockup = 'Home Component';
+
+  useEffect(() => {
+    setTitle(titleMockup);
+  }, []);
+
   return (
-    <Box
-      sx={{
-        width: '99.9%',
-        textAlign: 'center',
-        minHeight: '100vh',
-        overflow: 'hidden',
-      }}>
-      {/* Slider component */}
+    <>
+      <Title titleText={title} />
       <Box
         sx={{
-          width: '100%',
-          objectFit: 'fill',
+          width: '99.9%',
+          textAlign: 'center',
+          minHeight: '100vh',
           overflow: 'hidden',
-          maxHeight: '500px',
         }}>
-        <img src="https://picsum.photos/3200/1500" width="100%" />
+        <div name="sliders" />
+        <Container sx={{ marginTop: '50px' }}>
+          <Box sx={{ objectFit: 'contain' }}>
+            <Typography variant="h4">Últimas novedades</Typography>
+            <div name="news" />
+          </Box>
+        </Container>
       </Box>
-      <Container sx={{ marginTop: '20px' }}>
-        <Typography variant="h3">Texto de bienvenida</Typography>
-      </Container>
-      <Container sx={{ marginTop: '50px' }}>
-        <Box sx={{ objectFit: 'contain' }}>
-          <Typography variant="h4">Últimas novedades</Typography>
-          {/* UltimasNovedades Component */}
-          <img src="https://picsum.photos/1500/800" width="100%" />
-        </Box>
-      </Container>
-    </Box>
+    </>
   );
 };
 

b80726893cec5e95da26d986ccb8e801eea3e61b
AlVARO BARREIRA <alvarobarreiradev@gmail.com>
2021-12-14 00:05:07 -0300

    Correction, console.log eliminated

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -25,8 +25,6 @@ export const privatePATCH = async (path, id, body) => {
   try {
     const response = await axios.patch(`${path}/${id}`, body, config);
 
-    console.log(response.data);
-
     return response.data;
   } catch (error) {
     console.error(error);

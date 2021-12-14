8eca8adbffb5a8b416f3e93275db8c40ceb3c6c4
pablo Vieyra <pablin_vieyra1@outlook.com>
2021-12-14 14:28:12 -0300

    conflict resolution

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -18,10 +18,15 @@ export const privateDelete = async (path, id) => {
   try {
     const response = await axios.delete(`${path}/${id}`, config);
 
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 export const privatePUT = async (path, id, body) => {
   try {
     const response = await axios.put(`${path}/${id}`, body, config);
-
 
     return response.data;
   } catch (error) {

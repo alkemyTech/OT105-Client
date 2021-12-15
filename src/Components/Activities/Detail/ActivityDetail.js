100abd65e6295f04f28fa6fc28469dbbc3057097
Gabriel Shimabuku <gaby.shima12@gmail.com>
2021-12-14 22:03:01 -0300

    Removed unnecessary comments

--- src/Components/Activities/Detail/ActivityDetail.js
+++ src/Components/Activities/Detail/ActivityDetail.js
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from 'react';
-//COMPONENTS
 import Title from '../../Title/Title';
-//MUI
 import { CircularProgress, Container, Typography } from '@mui/material';
 import { getActivityById } from '../../../Services/activitiesService';
 

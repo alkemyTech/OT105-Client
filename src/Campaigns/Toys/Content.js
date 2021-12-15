9b7e7edd1eb98a590e58de1c08d0ed19edcf914c
Gabriel Shimabuku <gaby.shima12@gmail.com>
2021-12-15 20:52:27 -0300

    Created src/Styles/ToysCampaign directory and added Content.module.css . Imported .css file from Content.js component.

--- src/Campaigns/Toys/Content.js
+++ src/Campaigns/Toys/Content.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import s from '../../Styles/ToysCampaign/Content.module.css';
 
 const Content = () => {
-  return <div />;
+  return <div>This is content</div>;
 };
 
 export default Content;

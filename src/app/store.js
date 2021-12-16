7e40418a256ee22555467f77f7bd2663707d1538
Gabriel Shimabuku <gaby.shima12@gmail.com>
2021-12-16 05:44:15 -0300

    Added usReducer in redux store. Created usSlice.js in src/features/us directory. Created fetchUsData async thunk, which brings organization data and members list.

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 import membersReducer from '../features/members/membersSlice';
+import usReducer from '../features/us/usSlice';
 
 export default configureStore({
   reducer: {
     counter: counterReducer,
     members: membersReducer,
+    us: usReducer,
   },
 });

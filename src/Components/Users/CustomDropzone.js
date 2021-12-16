891393cfe3393ae5d09a2d13d051265066b4b30b
Julio <julio.franco9111@gmail.com>
2021-12-16 01:11:34 -0300

    create style const in Styles for custom dropzone

--- src/Components/Users/CustomDropzone.js
+++ src/Components/Users/CustomDropzone.js
@@ -2,7 +2,8 @@ import { Grid } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import { useDropzone } from 'react-dropzone';
 import { Alert } from '@mui/material';
-import { blobToBase64 } from '../../utils/imageToBase64';
+import { blobToBase64 } from '../../Utils/imageToBase64';
+import { styleCustomDropzone } from '../../Styles/UsersForm/styleCustomDropzone';
 
 export const CustomDropzone = ({ setImage64 = () => {} }) => {
   const [image, setImage] = useState({
@@ -84,28 +85,12 @@ export const CustomDropzone = ({ setImage64 = () => {} }) => {
     setImage64(base64);
   };
 
-  const style = {
-    flex: 1,
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    padding: '20px',
-    borderWidth: 2,
-    borderRadius: 2,
-    borderColor: '#eeeeee',
-    borderStyle: 'dashed',
-    backgroundColor: '#fafafa',
-    color: '#bdbdbd',
-    outline: 'none',
-    transition: 'border .24s ease-in-out',
-  };
-
   return (
     <>
       <Grid container spacing={3}>
         <Grid item md={8} xs={12}>
           <section className="container">
-            <div {...getRootProps({ style })}>
+            <div {...getRootProps({ style: styleCustomDropzone })}>
               <input {...getInputProps()} />
               <h3>Drag and drop some files here, or click to select files *</h3>
               <p>Only png and jpg accepted*</p>

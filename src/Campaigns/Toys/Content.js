00cb0d1621bbdd51e0aee29b727241639cb4a716
Gabriel Shimabuku <gaby.shima12@gmail.com>
2021-12-16 00:49:07 -0300

    Improved styles for accesibility

--- src/Campaigns/Toys/Content.js
+++ src/Campaigns/Toys/Content.js
@@ -61,7 +61,7 @@ const Content = () => {
             paddingBlock: '1.2rem',
             position: 'relative',
             zIndex: '100',
-            backgroundColor: 'rgba(250,250,250,0.8)',
+            backgroundColor: 'rgba(250,250,250,0.6)',
             ['@media (min-width:768px)']: {
               padding: '1.6rem',
               fontSize: '1.4rem',

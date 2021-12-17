bd1cea024783d6898ae7d543e19b48e5f5c6d930
pdvallejos <82845564+pdvallejos@users.noreply.github.com>
2021-12-17 11:24:38 -0300

    correccion de textos

--- src/Components/AlertDialog/AlertDialog.js
+++ src/Components/AlertDialog/AlertDialog.js
@@ -6,7 +6,7 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 
-export default function AlertDialog({ history }) {
+export default function AlertDialog({ message }) {
   const [open, setOpen] = React.useState(true);
 
   const handleClose = () => {
@@ -20,10 +20,10 @@ export default function AlertDialog({ history }) {
         aria-labelledby="alert-dialog-title"
         open={open}
         onClose={handleClose}>
-        <DialogTitle id="alert-dialog-title">{'Alert Error!!!'}</DialogTitle>
+        <DialogTitle id="alert-dialog-title">{'errorMessage'}</DialogTitle>
         <DialogContent>
           <DialogContentText id="alert-dialog-description">
-            {history}
+            {message}
           </DialogContentText>
         </DialogContent>
         <DialogActions>

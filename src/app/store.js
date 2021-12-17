0c932817a400779da3240075089e748773aeb77a
pdvallejos <82845564+pdvallejos@users.noreply.github.com>
2021-12-17 19:21:16 -0300

    creacion de reducer de los Slide

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import slidesSlice from '../Store/reducerSlice';
 
 export default configureStore({
   reducer: {
     counter: counterReducer,
+    slice: slidesSlice,
   },
 });

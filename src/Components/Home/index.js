f5fa63997cd03d6cabcdd22de36567a6cdaa7d0d
AlVARO BARREIRA <alvarobarreiradev@gmail.com>
2021-12-17 00:42:08 -0300

    Show alert and loader spinner in home page added

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -3,33 +3,53 @@ import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import { Typography } from '@mui/material';
 import Title from '../Title/Title';
+import LoaderSpinner from '../CommonComponents/LoaderSpinner';
+import { errorAlert } from '../../Services/alertsService';
 
 const HomeScreen = () => {
   const [title, setTitle] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
   const titleMockup = 'Home Component';
+
+  const loader = () => {
+    try {
+      setTimeout(() => setLoading(false), 2000);
+    } catch {
+      errorAlert();
+      setError(true);
+    }
+  };
 
   useEffect(() => {
     setTitle(titleMockup);
+    loader();
   }, []);
 
   return (
     <>
       <Title titleText={title} />
-      <Box
-        sx={{
-          width: '99.9%',
-          textAlign: 'center',
-          minHeight: '100vh',
-          overflow: 'hidden',
-        }}>
-        <div name="sliders" />
-        <Container sx={{ marginTop: '50px' }}>
-          <Box sx={{ objectFit: 'contain' }}>
-            <Typography variant="h4">Últimas novedades</Typography>
-            <div name="news" />
+      {loading ? (
+        <LoaderSpinner />
+      ) : (
+        !error && (
+          <Box
+            sx={{
+              width: '99.9%',
+              textAlign: 'center',
+              minHeight: '100vh',
+              overflow: 'hidden',
+            }}>
+            <div name="sliders" />
+            <Container sx={{ marginTop: '50px' }}>
+              <Box sx={{ objectFit: 'contain' }}>
+                <Typography variant="h4">Últimas novedades</Typography>
+                <div name="news" />
+              </Box>
+            </Container>
           </Box>
-        </Container>
-      </Box>
+        )
+      )}
     </>
   );
 };

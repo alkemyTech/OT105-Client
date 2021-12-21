12a398317726dbb2f8d8a5e84ac3350acd75faae
josejpm <josejpm@gmail.com>
2021-12-21 10:52:22 -0300

    debounce logig applied to Search component

--- src/Components/Categories/SearchForm/CategoriesSearchForm.js
+++ src/Components/Categories/SearchForm/CategoriesSearchForm.js
@@ -1,7 +1,43 @@
 import React from 'react';
+import TextField from '@mui/material/TextField';
 
 const CategoriesSearchForm = () => {
-  return <div></div>;
+  const handleChange = (e) => {
+    if (e.target.value.length > 2) {
+      console.log('Busqueda por termino');
+    } else {
+      console.log('Busqueda generica');
+    }
+  };
+
+  const debounceFn = (fn) => {
+    let timeoutId;
+
+    return function () {
+      if (timeoutId) {
+        clearTimeout(timeoutId);
+      }
+      const context = this;
+      const args = arguments;
+
+      timeoutId = setTimeout(() => {
+        fn.apply(context, args);
+      }, 500);
+    };
+  };
+
+  const debouncedHandleChange = debounceFn(handleChange);
+
+  return (
+    <div>
+      <TextField
+        id="outlined-basic"
+        label="Buscar categoria"
+        variant="outlined"
+        onChange={debouncedHandleChange}
+      />
+    </div>
+  );
 };
 
 export default CategoriesSearchForm;

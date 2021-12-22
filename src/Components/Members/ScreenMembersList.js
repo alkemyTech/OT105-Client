a7e5c2981ea60973bf50eeaca2b646aadf28b779
Gabriel Shimabuku <gaby.shima12@gmail.com>
2021-12-21 22:10:30 -0300

    Added getMembersByKeyword function in membersService.js . Removed mocked data from ScreenMembersList.js . Data is now given by SearchMembersForm.js, which makes an API query with debounced effect

--- src/Components/Members/ScreenMembersList.js
+++ src/Components/Members/ScreenMembersList.js
@@ -20,72 +20,25 @@ import {
 
 import AddIcon from '@mui/icons-material/Add';
 
-const exampleMembersData = {
-  success: true,
-  data: [
-    {
-      id: 258,
-      name: 'Osvaldo Olivera',
-      image: 'http://ongapi.alkemy.org/storage/kw2hNujhuq.jpeg',
-      description: 'Presidente',
-      facebookUrl: 'https://www.facebook.com/100075440371054/',
-      linkedinUrl: 'https://www.linkedin.com/in/osvaldo-olivera-785b78226/',
-      created_at: '2021-11-09T20:22:05.000000Z',
-      updated_at: '2021-11-23T04:34:29.000000Z',
-      deleted_at: null,
-      group_id: null,
-    },
-    {
-      id: 259,
-      name: 'Nahuel Narváez',
-      image: 'http://ongapi.alkemy.org/storage/z5x07SOhzl.jpeg',
-      description: 'Secretario',
-      facebookUrl: 'https://www.facebook.com/100075448260572/',
-      linkedinUrl: 'https://www.linkedin.com/in/nahuel-narváez-099b89226/',
-      created_at: '2021-11-09T23:25:38.000000Z',
-      updated_at: '2021-11-23T04:18:18.000000Z',
-      deleted_at: null,
-      group_id: null,
-    },
-    {
-      id: 261,
-      name: 'Griselda Germán',
-      image: 'http://ongapi.alkemy.org/storage/dR97wbZyUN.jpeg',
-      description: 'Tesorera',
-      facebookUrl: 'https://www.facebook.com/100074894726439/',
-      linkedinUrl: 'https://www.linkedin.com/in/griselda-germán-137b83226/',
-      created_at: '2021-11-10T00:32:23.000000Z',
-      updated_at: '2021-11-23T04:31:58.000000Z',
-      deleted_at: null,
-      group_id: null,
-    },
-  ],
-  message: 'Members retrieved successfully',
-};
-
-const membersMock = () => {
-  return new Promise((res) => {
-    setTimeout(() => res(exampleMembersData), 2000);
-  });
-};
-
 const ScreenMembersList = () => {
   const [members, setMembers] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
-    setIsLoading(true);
-    membersMock().then((res) => {
-      console.log(res.data);
-      setMembers(res.data);
-      setIsLoading(false);
-    });
-  }, []);
+  const updateLoadingState = (loadingState) => {
+    setIsLoading(loadingState);
+  };
+
+  const updateMembersList = (updatedMembers) => {
+    setMembers(updatedMembers);
+  };
 
   return (
     <>
       <Container maxWidth="md" sx={{ marginTop: '2rem' }}>
-        <SearchMembersForm />
+        <SearchMembersForm
+          updateLoadingState={updateLoadingState}
+          updateMembersList={updateMembersList}
+        />
         <Box
           alignItems="center"
           display="flex"
--- src/Components/Members/SearchMembersForm.js
+++ src/Components/Members/SearchMembersForm.js
@@ -1,29 +1,72 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import Paper from '@mui/material/Paper';
 import InputBase from '@mui/material/InputBase';
 import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
 import SearchIcon from '@mui/icons-material/Search';
+import {
+  getAllMembers,
+  getMembersByKeyword,
+} from '../../Services/membersService';
 
-export const SearchMembersForm = () => {
-  const handleChange = () => {
-    console.log('handling change');
+function useDebounce(value, delay) {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+      console.log(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+}
+
+export const SearchMembersForm = ({
+  updateMembersList,
+  updateLoadingState,
+}) => {
+  const [keywords, setKeywords] = useState('');
+
+  const handleChange = (e) => {
+    setKeywords(e.target.value);
   };
+
+  const debouncedValue = useDebounce(keywords, 500);
+
+  useEffect(() => {
+    updateLoadingState(true);
+    if (debouncedValue.length < 3) {
+      getAllMembers().then((res) => {
+        updateMembersList(res);
+        updateLoadingState(false);
+      });
+    } else {
+      getMembersByKeyword(keywords).then((res) => {
+        updateMembersList(res);
+        updateLoadingState(false);
+      });
+    }
+  }, [debouncedValue]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('handling submit');
+    setKeywords(e.target.value);
   };
 
   return (
     <Paper
       component="form"
       sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}
-      onSubmit={(e) => handleSubmit}>
+      onSubmit={(e) => handleSubmit(e)}>
       <InputBase
         placeholder="Buscar miembros..."
         sx={{ ml: 1, flex: 1 }}
-        onChange={handleChange}
+        onChange={(e) => handleChange(e)}
       />
       <Divider orientation="vertical" sx={{ height: 28, m: 0.5 }} />
       <IconButton aria-label="search" sx={{ p: '10px' }} type="submit">

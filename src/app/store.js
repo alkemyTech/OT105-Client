a7027344417e4c951789d838f2a66deeb5d6d3f1
AlVARO BARREIRA <alvarobarreiradev@gmail.com>
2021-12-30 11:09:38 -0300

    Create reducer for get users

--- src/app/store.js
+++ src/app/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 import membersReducer from '../features/members/membersSlice';
 import usReducer from '../features/us/usSlice';
+import usersReducer from '../features/users/usersSlice';
 
 export default configureStore({
   reducer: {
     counter: counterReducer,
     members: membersReducer,
     us: usReducer,
+    users: usersReducer,
   },
 });

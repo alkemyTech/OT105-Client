1a5e7c621f38b747dc0736f6d82fd6ce5f97a4fa
Gabriel Shimabuku <gaby.shima12@gmail.com>
2021-12-31 04:44:46 -0300

    Moved footerToys.css from src/Styles to src/Styles/ToysCampaign. footerToys is now a module (footerToys.module.css). Updated imports and classnames accordingly

--- src/Campaigns/Toys/Footer.js
+++ src/Campaigns/Toys/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../../Styles/footerToys.css';
+import s from '../../Styles/ToysCampaign/footerToys.module.css';
 import Fab from '@mui/material/Fab';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import InstagramIcon from '@mui/icons-material/Instagram';
@@ -10,32 +10,33 @@ const Footer = () => {
   return (
     <div>
       <footer className="bg-light text-center text-white">
-        <div className="sections">
-          <p className="col text-dark">Noticias</p>
+        <div className={s.sections}>
+          <p className={s.p}>Noticias</p>
 
-          <p className="col text-dark">Actividades</p>
+          <p className={s.p}>Actividades</p>
 
-          <p className="col text-dark">Novedades</p>
+          <p className={s.p}>Novedades</p>
 
           <a href="/">
             <img
               alt=""
+              className={s.logo}
               src={'http://ongapi.alkemy.org/storage/4ZR8wsUwr9.png'}
             />
           </a>
 
-          <p className="col text-dark">Testimonios</p>
+          <p className={s.p}>Testimonios</p>
 
-          <p className="col text-dark">Nosotros</p>
+          <p className={s.p}>Nosotros</p>
 
-          <p className="col text-dark">Contacto</p>
+          <p className={s.p}>Contacto</p>
         </div>
 
         <div classNameName="container">
           <hr className="bg-dark border-1 border-top border-dark" />
         </div>
 
-        <div className="sections">
+        <div className={s.sections}>
           <section className="mb-4">
             <Fab
               aria-label="add"
@@ -87,7 +88,7 @@ const Footer = () => {
           </section>
         </div>
 
-        <div className="sections">
+        <div className={s.sections}>
           © 2021 by Alkemy{' '}
           <a className="text-black" href="https://alkemy.org">
             All rights reserved.

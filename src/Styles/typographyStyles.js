7e3e4c6e3bf8eb47d3cb88bf902c143f95c03b9a
Gabriel Shimabuku <gaby.shima12@gmail.com>
2021-12-31 06:14:28 -0300

    Created BackofficeDasboard.module.css to store BackofficeDasboard styles. Improved typographyStyles to fit on smaller screens. BackofficeDasboard.js: Removed user.name Typography. Now renders a BackofficeDashboardCard component for every section in backoffice

--- src/Styles/typographyStyles.js
+++ src/Styles/typographyStyles.js
@@ -4,4 +4,8 @@ export const paragraphText40 = {
   marginTop: 20,
 };
 
-export const paragraphText45 = { fontSize: 45, textAlign: 'center' };
+export const paragraphText45 = {
+  fontSize: { xs: '1.7rem', sm: 45 },
+  textAlign: 'center',
+  marginBottom: '2rem',
+};

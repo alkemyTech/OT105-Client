dac72c0a1e189013750890d90f94ceb8f12bd792
josejpm <josejpm@gmail.com>
2022-01-02 11:56:41 -0300

    newsDetails connected with newsService

--- src/Components/News/Detail/NewsDetail.js
+++ src/Components/News/Detail/NewsDetail.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router';
+import ReactHtmlParser from 'react-html-parser';
 import { Typography, Box } from '@mui/material';
 import Title from '../../Title/Title';
 import style from '../../../Styles/NewsDetail/NewsDetail.module.css';
 import { getNewsById } from '../../../Services/NewsService';
+import newsImage from '../../../assets/img/newsBackG_S.jpg';
 
 const NewsDetail = ({ newsTitle }) => {
   const { id } = useParams();
@@ -22,11 +24,11 @@ const NewsDetail = ({ newsTitle }) => {
 
   return (
     <div>
-      <Title imageUrl={newsDetail.image} titleText={newsTitle} />
+      <Title imageUrl={newsImage} titleText={newsDetail.name} />
       <Box className={style.newsDetail__container} component="div">
         <Box className={style.newsDetail__content} component="div">
           <Typography component="p" variant="p">
-            {newsDetail.content}
+            {ReactHtmlParser(newsDetail.content)}
           </Typography>
         </Box>
         <Box className={style.newsDetail__imageContainer} component="div">

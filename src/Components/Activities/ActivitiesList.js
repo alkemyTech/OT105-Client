f40e173811b8f04ccb3f2bc2d0dad581d022c34e
Facundo Benitez <facundob91@gmail.com>
2022-01-02 17:04:31 -0300

    add loading visual to activities list

--- src/Components/Activities/ActivitiesList.js
+++ src/Components/Activities/ActivitiesList.js
@@ -1,32 +1,48 @@
 import React, { useState, useEffect } from 'react';
 import '../CardListStyles.css';
+import Spinner from '../CommonComponents/LoaderSpinner';
 import { getActivities } from '../../Services/ActivitiesServices';
+import { Link } from 'react-router-dom';
+
 const ActivitiesList = () => {
   const [activities, setActivities] = useState([{}]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    getActivities().then((resp) => {
-      setActivities(resp);
-    });
+    const fetchData = async () => {
+      setIsLoading(true);
+      const response = await getActivities();
+
+      setActivities(response);
+      setIsLoading(false);
+    };
+
+    fetchData();
   }, []);
 
   return (
     <div>
       <h1>Listado Actividades</h1>
-      <ul className="list-container">
-        {activities.length > 0 ? (
-          activities.map((activity) => {
-            return (
-              <li key={activity.id} className="card-info">
-                <h3>{activity.name}</h3>
-                <p>{activity.description}</p>
-              </li>
-            );
-          })
-        ) : (
-          <p>No hay actividades</p>
-        )}
-      </ul>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <ul className="list-container">
+          {activities.length > 0 ? (
+            activities.map((activity) => {
+              return (
+                <li key={activity.id} className="card-info">
+                  <Link to={`/actividades/${activity.id}`}>
+                    <h3>{activity.name}</h3>
+                    <p>{activity.description}</p>
+                  </Link>
+                </li>
+              );
+            })
+          ) : (
+            <p>No hay actividades</p>
+          )}
+        </ul>
+      )}
     </div>
   );
 };

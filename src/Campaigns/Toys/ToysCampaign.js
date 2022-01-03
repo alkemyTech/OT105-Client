288febd96374c1293a6887c232d8216e0470170d
Gabriel Shimabuku <gaby.shima12@gmail.com>
2022-01-03 05:48:57 -0300

    Added BackToHomeButton.js in ToysCampaign & SchoolCampaign

--- src/Campaigns/Toys/ToysCampaign.js
+++ src/Campaigns/Toys/ToysCampaign.js
@@ -3,10 +3,12 @@ import Header from './Header';
 import Slider from './Slider';
 import Content from './Content';
 import Footer from './Footer';
+import BackToHomeButton from '../BackToHomeButton';
 
 const ToysCampaign = () => {
   return (
     <>
+      <BackToHomeButton />
       <Header />
       <Slider />
       <Content />

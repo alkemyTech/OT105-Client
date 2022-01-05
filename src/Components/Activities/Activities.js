7c49c1f6cd50b986cfaf09c0f79b56df138512b8
Facundo Benitez <facundob91@gmail.com>
2022-01-05 03:33:10 -0300

    add background image to title

--- src/Components/Activities/Activities.js
+++ src/Components/Activities/Activities.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Title from '../Title/Title';
 import ActivitiesList from './ActivitiesList';
+import photo from '../../assets/foto9.jpg';
 
 function Activities() {
   return (
     <div>
-      <Title titleText={'Actividades'} />
+      <Title imageUrl={photo} titleText={'Actividades'} />
       <ActivitiesList />
     </div>
   );
